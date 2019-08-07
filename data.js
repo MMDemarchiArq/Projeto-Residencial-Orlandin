090d050ec6d0818804584f474999f58adbffab2b
MMDemarchiArq <50173122+MMDemarchiArq@users.noreply.github.com>
2019-08-07 13:14:20 -0300

    Orlandin

--- data.js
+++ data.js
@@ -33,19 +33,19 @@ var APP_DATA = {
           "yaw": -1.7851547013383726,
           "pitch": 0.2128685544013944,
           "rotation": 0,
-          "target": "14-sala-de-estar-vista-1"
+          "target": "13-sala-de-estar-vista-1"
         },
         {
           "yaw": -1.2821921875582838,
           "pitch": 0.19482620792787486,
           "rotation": 0,
-          "target": "10-garagem"
+          "target": "9-garagem"
         },
         {
           "yaw": -2.0682359898838243,
           "pitch": 0.1388116900477563,
           "rotation": 0,
-          "target": "22-corredor-lateral-esquerdo-1"
+          "target": "21-corredor-lateral-esquerdo-1"
         }
       ],
       "infoHotspots": []
@@ -89,7 +89,7 @@ var APP_DATA = {
           "yaw": -0.9202904362795294,
           "pitch": 0.32031896860481623,
           "rotation": 1.5707963267948966,
-          "target": "10-garagem"
+          "target": "9-garagem"
         },
         {
           "yaw": -2.4497488798186726,
@@ -101,7 +101,7 @@ var APP_DATA = {
           "yaw": -1.4892543066054387,
           "pitch": 0.7240954616424684,
           "rotation": 2.356194490192345,
-          "target": "16-sala-de-jantar"
+          "target": "15-sala-de-jantar"
         }
       ],
       "infoHotspots": []
@@ -145,7 +145,7 @@ var APP_DATA = {
           "yaw": -1.1985166235547116,
           "pitch": 0.9146590585820302,
           "rotation": 3.141592653589793,
-          "target": "28-entrada-sute"
+          "target": "27-entrada-sute"
         }
       ],
       "infoHotspots": []
@@ -183,7 +183,7 @@ var APP_DATA = {
           "yaw": -0.9354418734074983,
           "pitch": 0.7115022032091307,
           "rotation": 7.853981633974483,
-          "target": "28-entrada-sute"
+          "target": "27-entrada-sute"
         }
       ],
       "infoHotspots": []
@@ -218,28 +218,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.1414697518868042,
-          "pitch": 0.3384401135673194,
+          "yaw": -1.1291461110158956,
+          "pitch": 0.4803204478468519,
           "rotation": 1.5707963267948966,
-          "target": "8-escritrio"
+          "target": "29-escritrio"
         },
         {
           "yaw": -1.7209986618461244,
           "pitch": 0.20451418532121224,
           "rotation": 1.5707963267948966,
-          "target": "29-banheiro"
+          "target": "28-banheiro"
         },
         {
           "yaw": -2.080522127240501,
           "pitch": 0.12141851223810818,
           "rotation": 1.5707963267948966,
-          "target": "26-quarto-1-vista-1"
+          "target": "25-quarto-1-vista-1"
         },
         {
           "yaw": -2.2892704804701154,
           "pitch": 0.11176784322986144,
           "rotation": 0,
-          "target": "28-entrada-sute"
+          "target": "27-entrada-sute"
         },
         {
           "yaw": -1.955891957347422,
@@ -333,13 +333,13 @@ var APP_DATA = {
           "yaw": -1.5022136128712447,
           "pitch": 0.41111505682054883,
           "rotation": 1.5707963267948966,
-          "target": "9-espao-gourmet"
+          "target": "8-espao-gourmet"
         },
         {
           "yaw": -2.432595202237046,
           "pitch": 0.28820156612482783,
           "rotation": 4.71238898038469,
-          "target": "11-lavanderia"
+          "target": "10-lavanderia"
         }
       ],
       "infoHotspots": []
@@ -377,51 +377,13 @@ var APP_DATA = {
           "yaw": -2.5727601574181467,
           "pitch": 0.28255857131821216,
           "rotation": 4.71238898038469,
-          "target": "11-lavanderia"
+          "target": "10-lavanderia"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-escritrio",
-      "name": "Escritório",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -1.3923994676324156,
-        "pitch": 0.4048018535292428,
-        "fov": 1.2691192001910299
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.43989282334630886,
-          "pitch": 0.4930521039296636,
-          "rotation": 1.5707963267948966,
-          "target": "4-corredor"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "9-espao-gourmet",
+      "id": "8-espao-gourmet",
       "name": "Espaço Gourmet",
       "levels": [
         {
@@ -459,13 +421,13 @@ var APP_DATA = {
           "yaw": -2.3555077094210546,
           "pitch": 0.32409554834495324,
           "rotation": 0,
-          "target": "12-mesa-espao-gourmet"
+          "target": "11-mesa-espao-gourmet"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-garagem",
+      "id": "9-garagem",
       "name": "Garagem",
       "levels": [
         {
@@ -509,13 +471,13 @@ var APP_DATA = {
           "yaw": -1.1565473330871558,
           "pitch": 0.15938684810943649,
           "rotation": 0,
-          "target": "20-corredor-lateral-direito-1"
+          "target": "19-corredor-lateral-direito-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-lavanderia",
+      "id": "10-lavanderia",
       "name": "Lavanderia",
       "levels": [
         {
@@ -559,13 +521,13 @@ var APP_DATA = {
           "yaw": -1.6335078424416913,
           "pitch": 0.8034500285176236,
           "rotation": 3.141592653589793,
-          "target": "23-corredor-lateral-esquerdo-2"
+          "target": "22-corredor-lateral-esquerdo-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "12-mesa-espao-gourmet",
+      "id": "11-mesa-espao-gourmet",
       "name": "Mesa Espaço Gourmet",
       "levels": [
         {
@@ -597,31 +559,31 @@ var APP_DATA = {
           "yaw": -0.8766402076895581,
           "pitch": 0.24080505969896215,
           "rotation": 0,
-          "target": "9-espao-gourmet"
+          "target": "8-espao-gourmet"
         },
         {
           "yaw": -1.6155649924233497,
           "pitch": 0.7568324498079768,
           "rotation": 3.9269908169872414,
-          "target": "13-piscina"
+          "target": "12-piscina"
         },
         {
           "yaw": -2.4752497452575284,
           "pitch": 0.04548998999880993,
           "rotation": 10.995574287564278,
-          "target": "25-lavabo"
+          "target": "24-lavabo"
         },
         {
           "yaw": -0.6399596677130184,
           "pitch": 0.18017126015434926,
           "rotation": 0,
-          "target": "23-corredor-lateral-esquerdo-2"
+          "target": "22-corredor-lateral-esquerdo-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "13-piscina",
+      "id": "12-piscina",
       "name": "Piscina",
       "levels": [
         {
@@ -653,19 +615,19 @@ var APP_DATA = {
           "yaw": -0.6093597405566129,
           "pitch": 0.7486991419724287,
           "rotation": 2.356194490192345,
-          "target": "12-mesa-espao-gourmet"
+          "target": "11-mesa-espao-gourmet"
         },
         {
           "yaw": -0.8001299601047052,
           "pitch": 0.21688075279446295,
           "rotation": 7.853981633974483,
-          "target": "24-fundos"
+          "target": "23-fundos"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "14-sala-de-estar-vista-1",
+      "id": "13-sala-de-estar-vista-1",
       "name": "Sala de Estar Vista 1",
       "levels": [
         {
@@ -703,19 +665,19 @@ var APP_DATA = {
           "yaw": -1.1715226043886524,
           "pitch": 0.35964921918942316,
           "rotation": 0,
-          "target": "16-sala-de-jantar"
+          "target": "15-sala-de-jantar"
         },
         {
           "yaw": -2.0688724171480697,
           "pitch": 0.40885261419443,
           "rotation": 4.71238898038469,
-          "target": "15-sala-de-estar-vista-2"
+          "target": "14-sala-de-estar-vista-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "15-sala-de-estar-vista-2",
+      "id": "14-sala-de-estar-vista-2",
       "name": "Sala de Estar Vista 2",
       "levels": [
         {
@@ -753,19 +715,19 @@ var APP_DATA = {
           "yaw": -1.3673756781508217,
           "pitch": 0.4613747120035647,
           "rotation": 6.283185307179586,
-          "target": "14-sala-de-estar-vista-1"
+          "target": "13-sala-de-estar-vista-1"
         },
         {
           "yaw": -1.6099764504046519,
           "pitch": 0.1321802050001626,
           "rotation": 4.71238898038469,
-          "target": "16-sala-de-jantar"
+          "target": "15-sala-de-jantar"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "16-sala-de-jantar",
+      "id": "15-sala-de-jantar",
       "name": "Sala de Jantar",
       "levels": [
         {
@@ -797,7 +759,7 @@ var APP_DATA = {
           "yaw": -2.704562688463609,
           "pitch": 0.1291500495917255,
           "rotation": 4.71238898038469,
-          "target": "14-sala-de-estar-vista-1"
+          "target": "13-sala-de-estar-vista-1"
         },
         {
           "yaw": -1.1036171650935032,
@@ -809,7 +771,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "17-suite-vista-1",
+      "id": "16-suite-vista-1",
       "name": "Suite Vista 1",
       "levels": [
         {
@@ -841,31 +803,31 @@ var APP_DATA = {
           "yaw": -1.908546686894443,
           "pitch": 0.06732666011604138,
           "rotation": 0,
-          "target": "19-banheiro-da-sute"
+          "target": "18-banheiro-da-sute"
         },
         {
           "yaw": -1.2608441459671038,
           "pitch": 0.10058470034622324,
           "rotation": 1.5707963267948966,
-          "target": "21-corredor-lateral-direito-2"
+          "target": "20-corredor-lateral-direito-2"
         },
         {
           "yaw": -2.4598881427490706,
           "pitch": 0.5390413956915321,
           "rotation": 1.5707963267948966,
-          "target": "18-sute-vista-2"
+          "target": "17-sute-vista-2"
         },
         {
           "yaw": -1.8071839082594607,
           "pitch": 0.8483703896304355,
           "rotation": 3.141592653589793,
-          "target": "28-entrada-sute"
+          "target": "27-entrada-sute"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "18-sute-vista-2",
+      "id": "17-sute-vista-2",
       "name": "Suíte Vista 2",
       "levels": [
         {
@@ -897,19 +859,19 @@ var APP_DATA = {
           "yaw": -2.5814224551327474,
           "pitch": 0.09273258632140724,
           "rotation": 4.71238898038469,
-          "target": "21-corredor-lateral-direito-2"
+          "target": "20-corredor-lateral-direito-2"
         },
         {
           "yaw": -1.3906327105594478,
           "pitch": 0.31063758875967196,
           "rotation": 0,
-          "target": "28-entrada-sute"
+          "target": "27-entrada-sute"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "19-banheiro-da-sute",
+      "id": "18-banheiro-da-sute",
       "name": "Banheiro da Suíte",
       "levels": [
         {
@@ -941,13 +903,13 @@ var APP_DATA = {
           "yaw": -2.4978873111788698,
           "pitch": 0.32701554009969946,
           "rotation": 4.71238898038469,
-          "target": "17-suite-vista-1"
+          "target": "16-suite-vista-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "20-corredor-lateral-direito-1",
+      "id": "19-corredor-lateral-direito-1",
       "name": "Corredor Lateral Direito 1",
       "levels": [
         {
@@ -979,19 +941,19 @@ var APP_DATA = {
           "yaw": -1.6387777221335362,
           "pitch": 0.7338979648457347,
           "rotation": 3.141592653589793,
-          "target": "10-garagem"
+          "target": "9-garagem"
         },
         {
           "yaw": -1.6704985411594073,
           "pitch": 0.18913701160385088,
           "rotation": 0,
-          "target": "21-corredor-lateral-direito-2"
+          "target": "20-corredor-lateral-direito-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "21-corredor-lateral-direito-2",
+      "id": "20-corredor-lateral-direito-2",
       "name": "Corredor Lateral Direito 2",
       "levels": [
         {
@@ -1023,25 +985,25 @@ var APP_DATA = {
           "yaw": -1.676612466656536,
           "pitch": 0.6002369187577372,
           "rotation": 3.141592653589793,
-          "target": "20-corredor-lateral-direito-1"
+          "target": "19-corredor-lateral-direito-1"
         },
         {
           "yaw": -1.693345954415868,
           "pitch": 0.2214267018807572,
           "rotation": 0,
-          "target": "24-fundos"
+          "target": "23-fundos"
         },
         {
           "yaw": -2.375190845333208,
           "pitch": 0.07027410459520667,
           "rotation": 4.71238898038469,
-          "target": "17-suite-vista-1"
+          "target": "16-suite-vista-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "22-corredor-lateral-esquerdo-1",
+      "id": "21-corredor-lateral-esquerdo-1",
       "name": "Corredor Lateral Esquerdo 1",
       "levels": [
         {
@@ -1079,13 +1041,13 @@ var APP_DATA = {
           "yaw": -1.5973932806220077,
           "pitch": 0.18555325298533631,
           "rotation": 0,
-          "target": "23-corredor-lateral-esquerdo-2"
+          "target": "22-corredor-lateral-esquerdo-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "23-corredor-lateral-esquerdo-2",
+      "id": "22-corredor-lateral-esquerdo-2",
       "name": "Corredor Lateral Esquerdo 2",
       "levels": [
         {
@@ -1117,25 +1079,25 @@ var APP_DATA = {
           "yaw": -1.4337819679201331,
           "pitch": 0.7621202878612063,
           "rotation": 3.141592653589793,
-          "target": "22-corredor-lateral-esquerdo-1"
+          "target": "21-corredor-lateral-esquerdo-1"
         },
         {
           "yaw": -0.855735686772281,
           "pitch": 0.19990460200684979,
           "rotation": 1.5707963267948966,
-          "target": "11-lavanderia"
+          "target": "10-lavanderia"
         },
         {
           "yaw": -1.470658945480853,
           "pitch": 0.2055953006523712,
           "rotation": 0,
-          "target": "12-mesa-espao-gourmet"
+          "target": "11-mesa-espao-gourmet"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "24-fundos",
+      "id": "23-fundos",
       "name": "Fundos",
       "levels": [
         {
@@ -1167,19 +1129,19 @@ var APP_DATA = {
           "yaw": -2.002884440238157,
           "pitch": 0.3940146717101989,
           "rotation": 4.71238898038469,
-          "target": "13-piscina"
+          "target": "12-piscina"
         },
         {
           "yaw": -0.7667548256522867,
           "pitch": 0.7945338100101722,
           "rotation": 3.141592653589793,
-          "target": "21-corredor-lateral-direito-2"
+          "target": "20-corredor-lateral-direito-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "25-lavabo",
+      "id": "24-lavabo",
       "name": "Lavabo",
       "levels": [
         {
@@ -1211,13 +1173,13 @@ var APP_DATA = {
           "yaw": -2.3730395268900217,
           "pitch": 0.7366503713812396,
           "rotation": 3.9269908169872414,
-          "target": "12-mesa-espao-gourmet"
+          "target": "11-mesa-espao-gourmet"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "26-quarto-1-vista-1",
+      "id": "25-quarto-1-vista-1",
       "name": "Quarto 1 Vista 1",
       "levels": [
         {
@@ -1255,13 +1217,13 @@ var APP_DATA = {
           "yaw": -1.202868445893257,
           "pitch": 0.500749665395599,
           "rotation": 0,
-          "target": "27-quarto-1-vista-2"
+          "target": "26-quarto-1-vista-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "27-quarto-1-vista-2",
+      "id": "26-quarto-1-vista-2",
       "name": "Quarto 1 Vista 2",
       "levels": [
         {
@@ -1299,13 +1261,13 @@ var APP_DATA = {
           "yaw": -1.8612134400055567,
           "pitch": 0.4568953901670536,
           "rotation": 6.283185307179586,
-          "target": "26-quarto-1-vista-1"
+          "target": "25-quarto-1-vista-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "28-entrada-sute",
+      "id": "27-entrada-sute",
       "name": "Entrada Suíte",
       "levels": [
         {
@@ -1349,13 +1311,13 @@ var APP_DATA = {
           "yaw": -1.3052412233937805,
           "pitch": 0.4394268261767724,
           "rotation": 1.5707963267948966,
-          "target": "17-suite-vista-1"
+          "target": "16-suite-vista-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "29-banheiro",
+      "id": "28-banheiro",
       "name": "Banheiro",
       "levels": [
         {
@@ -1387,6 +1349,44 @@ var APP_DATA = {
           "yaw": -2.4641621274890824,
           "pitch": 0.4424651815745797,
           "rotation": 4.71238898038469,
+          "target": "4-corredor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "29-escritrio",
+      "name": "Escritório",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4117899362836166,
+        "pitch": 0.19680453785393226,
+        "fov": 1.1545084566471375
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7223222949406818,
+          "pitch": 0.6709344293425978,
+          "rotation": 3.141592653589793,
           "target": "4-corredor"
         }
       ],
